39ae827c55ef826b7bc9ca3e6298e7d65c945f26
AvrilSaa <avrilsaavedra@college.harvard.edu>
2020-03-01 08:52:29 -0800

    fixed filter

--- lab8_part2.ml
+++ lab8_part2.ml
@@ -116,13 +116,13 @@ module MakeStack (Element: SERIALIZE) : (STACK with type element = Element.t) =
     let pop (s : stack) : stack =
       snd (pop_helper s)
 
-    let map (f : element -> element) (s : stack) : stack =
-      List.map f s 
+    let map: (element -> element) -> stack -> stack =
+      List.map 
 
-    let filter (f : element -> bool) (s : stack) : stack =
-      List.filter f s 
+    let filter: (element -> bool) -> stack) -> stack =
+      List.filter
 
-    let fold_left (f : 'a -> element -> 'a) (init : 'a) (s : stack) : 'a =
+    let fold_left: ('a -> element -> 'a) -> 'a -> stack -> 'a =
       List.fold_left
 
     let rec serialize (s : stack) : string =
